eebfa2d46d84c9691a8bdb0db51b6ce8faee1aec
Tommy328 <jiabin0328@hotmail.com>
2020-12-18 17:42:25 -0500

    revert testing configurations

--- src/dataset.py
+++ src/dataset.py
@@ -92,8 +92,8 @@ class Dataset(torch.utils.data.Dataset):
         sigma = self.sigma
         # in test mode images are masked (with masked regions),
         # using 'mask' parameter prevents canny to detect edges for the masked regions
-        # mask = None if self.training else (1 - mask / 255).astype(np.bool)
-        mask = None
+        mask = None if self.training else (1 - mask / 255).astype(np.bool)
+        # mask = None
 
         # canny
         if self.edge == 1:
@@ -122,8 +122,8 @@ class Dataset(torch.utils.data.Dataset):
     def load_gradient(self, img, index, mask):
         # in test mode images are masked (with masked regions),
         # using 'mask' parameter prevents output to include masked regions
-        # mask = None if self.training else (1 - mask / 255).astype(np.bool)
-        mask = None
+        mask = None if self.training else (1 - mask / 255).astype(np.bool)
+        # mask = None
 
         filterx = np.array([[0, 0, 0],[-1, 0, 1],[0, 0, 0]])
         # Calculate gradients
@@ -132,7 +132,7 @@ class Dataset(torch.utils.data.Dataset):
         gradient = np.sqrt(np.square(Ix) + np.square(Iy))
 
         # remove masks if testing or sampling
-        if self.training:
+        if not self.training:
             new_mask = np.copy(mask)
             h, w = new_mask.shape[:2]
             for x in range(h):
